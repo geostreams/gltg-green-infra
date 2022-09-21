5e214693fad3206e1e138149a6b3b9cf466cf411
Vismayak Mohanarajan <vismayak97@gmail.com>
2022-09-21 16:15:08 -0500

    Updated database and added API

--- api/controllers/huc8.py
+++ api/controllers/huc8.py
@@ -1,5 +1,6 @@
 from flask import abort, jsonify, make_response
 from models.huc8 import HUC8, HUC8Schema
+from sqlalchemy import exc
 
 
 def huc8_get(page=None, limit=None):  # noqa: E501
@@ -14,9 +15,39 @@ def huc8_get(page=None, limit=None):  # noqa: E501
 
     :rtype: Pagination
     """
-    huc8s = HUC8.query.order_by(HUC8.name).all()
 
-    # Serialize the data for the response
-    huc8_schema = HUC8Schema(many=True)
-    data = huc8_schema.dump(huc8s)
-    return jsonify(data)
+    try:
+        huc8s = HUC8.query.order_by(HUC8.name).all()
+
+        # Serialize the data for the response
+        huc8_schema = HUC8Schema(many=True)
+
+        data = huc8_schema.dump(huc8s)
+        return jsonify({"response": 200, "results": data})
+    except exc.SQLAlchemyError:
+        return jsonify({"response": 500, "results": []})
+
+
+def huc8_get_id(huc8_id):  # noqa: E501
+    """Get a single HUC8 by id
+
+     # noqa: E501
+
+    :param huc8_id: HUC8 number
+    :type huc8_id: str
+
+    :rtype: HUC8
+    """
+
+    try:
+        huc8 = HUC8.query.filter_by(huc8=huc8_id).all()
+
+        # Serialize the data for the response
+        huc8_schema = HUC8Schema(many=True)
+        data = huc8_schema.dump(huc8)
+        if len(data) == 0:
+            return jsonify({"response": 400, "results": []})
+        else:
+            return jsonify({"response": 200, "results": [data]})
+    except exc.SQLAlchemyError:
+        return jsonify({"response": 500, "results": []})
